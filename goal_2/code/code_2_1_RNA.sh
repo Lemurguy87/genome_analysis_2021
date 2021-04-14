243049125afe2bb4ae8f18734615b66a46a15d76
Gustav Ahlström <guah1142@s21.uppmax.uu.se>
2021-04-14 17:00:14 +0200

    added RNA script

---
+++ goal_2/code/code_2_1_RNA.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 20:00:00
+#SBATCH -J 2_1_binning_RNA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user gustav.ahlstrom.1142@student.uu.se
+
+module load bioinfo-tools
+module load MetaBat
+
+megahit -1 /home/guah1142/genome_analysis_2021/results_RNA/forward_paired_137_1.fq.gz -2 /home/guah1142/genome_analysis_2021/results_RNA/forward_paired_137_2.fq.gz -o /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_2/2_1/  
+megahit -1 /home/guah1142/genome_analysis_2021/results_RNA/forward_paired_139_1.fq.gz -2 /home/guah1142/genome_analysis_2021/results_RNA/forward_paired_139_2.fq.gz -o /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_2/2_1/
+
