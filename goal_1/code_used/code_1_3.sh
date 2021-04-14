d7c709d8efd2574da308cd580387a062a8be604c
Gustav Ahlström <guah1142@s21.uppmax.uu.se>
2021-04-14 16:17:41 +0200

    updated code for timmomatic paths

--- goal_1/code_used/code_1_3.sh
+++ goal_1/code_used/code_1_3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_137_1.fastq.gz
-fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_137_2.fastq.gz
-fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_139_1.fastq.gz
-fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_139_2.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/forward_paired_137_1.fq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/reverse_paired_137_2.fq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/forward_paired_139_1.fq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/reverse_paired_139_2.fq.gz
 
 
 
