62b8e5b68561a6c42c9d097f0f5298583127b310
Gustav Ahlström <guah1142@rackham2.uppmax.uu.se>
2021-04-14 10:03:46 +0200

    added code

---
+++ goal_1/code_used/code_1_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/RNA_raw/SRR4342137.1.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/RNA_raw/SRR4342139.1.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/RNA_raw/SRR4342137.2.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/RNA_raw/SRR4342139.2.fastq.gz
+
+
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342133.1.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342129.1.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342133.2.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342129.2.fastq.gz
---
+++ goal_1/code_used/code_1_3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_137_1.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_137_2.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_139_1.fastq.gz
+fastqc /home/guah1142/genome_analysis_2021/results_RNA/trimmed_139_2.fastq.gz
+
+
+
