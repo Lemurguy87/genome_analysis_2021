46f661b6271aa526dca8aae56dd247b5b8d80930
Gustav Ahlström <guah1142@rackham3.uppmax.uu.se>
2021-04-15 16:36:16 +0200

    updated DNA code in accordance with new pp

--- goal_2/code/code_2_1_DNA.sh
+++ goal_2/code/code_2_1_DNA.sh
@@ -5,16 +5,15 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 20:00:00
-#SBATCH -J 2_1_binning
+#SBATCH -J 2_1_binning_DNA
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gustav.ahlstrom.1142@student.uu.se 
 
 module load bioinfo-tools 
-module load MetaBat 
+module load megahit
 
-#RNA trims 
-metabat -1 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342129_1.paired.trimmed.fastq.gz -2 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342129_2.paired.trimmed.fastq.gz -o DNA_2_1_bin_129 
-metabat -1 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342133_1.paired.trimmed.fastq.gz -2 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342133_2.paired.trimmed.fastq.gz -o DNA_2_1_bin_133 
-	
+#DNA trims 
+megahit -1 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342129_1.paired.trimmed.fastq.gz -2 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342129_2.paired.trimmed.fastq.gz -o /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_2/2_1/DNA_bins/	
+megahit -1 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_raw/SRR4342133_1.paired.trimmed.fastq.gz -2 /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_1/raw_data/DNA_rawSRR4342133_2.paired.trimmed.fastq.gz -o /home/guah1142/genome_analysis_2021/genome_analysis_2021/goal_2/2_1/DNA_bins/ 
 
 
